a780e303d40dd7e207084f4df737cc094aacdf73
XBigTK13X <xbitk13x@gmail.com>
2016-08-30 13:18:38 -0500

    Template rendering of recipe titles now working

--- source/main.py
+++ source/main.py
@@ -1,11 +1,31 @@
 from jinja2 import Environment, FileSystemLoader
 
+import yaml
+
 import os
 
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-templates = Environment(loader=FileSystemLoader(CURRENT_DIR))
-TEMPLATE_DIR = os.path.join(CURRENT_DIR,'../template')
+TEMPLATE_DIR = 'template'
+RECIPE_DIR = 'input'
+
+templateCache = {}
+templates = Environment(loader=FileSystemLoader('template'))
+# Preload all templates
 for root, dirs, files in os.walk(TEMPLATE_DIR):
+	for file in files:		
+		templateCache[file] = templates.get_template(file)
+
+payload = {
+	'recipes':[]
+}
+
+# Create a top-level directory of recipes for the index
+for root, dirs, files in os.walk(RECIPE_DIR):
 	for file in files:
-		print(file)
-		#print templates.get_template('test_template.html').render(payload=payload)
+		with open(os.path.join(root,file), 'r') as stream:
+			recipe = yaml.load(stream)
+			payload['recipes'].append(recipe)
+
+import pprint
+pprint.pprint(payload)
+
+print(templateCache['index.jinja'].render(payload=payload))
